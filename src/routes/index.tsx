3f525e844d9484fe1c0b77a7764eecbfbf459e5a
rdmbtc <ntraid03@gmail.com>
2026-06-09 07:33:07 +0500

    fix: import Command from cmdk to resolve client runtime ReferenceError

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,6 +14,7 @@ import { useRepoMeta } from "@/hooks/useRepoMeta";
 import Lenis from "lenis";
 import Magnetic from "@/components/Magnetic";
 import SplitTextReveal from "@/components/SplitTextReveal";
+import { Command } from "cmdk";
 
 import { toast, Toaster } from "sonner";
 
